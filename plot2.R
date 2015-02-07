0bf1212a42f7b1749c182630ea0cd48373234374
caseytomlin <caseytomlin@gmail.com>
2015-02-07 20:38:51 +0100

    new file:   plot1.R 	new file:   plot1.png 	new file:   plot2.R 	new file:   plot2.png 	new file:   plot3.R 	new file:   plot3.png 	new file:   plot4.R 	new file:   plot4.png

---
+++ plot2.R
@@ -0,0 +1,34 @@
+plot2 <- function() {
+    # Loads the data from the zip file url and reproduces plot2.png
+        
+    # If the zip file is not in the present working directory, download it.
+    # Then unzip (if necessary), and read the data into the dataframe data.
+    filename <- "household_power_consumption.txt"
+    zipname <- "exdata-data-household_power_consumption.zip"
+    if(!file.exists(filename)) {
+        if(!file.exists(zipname)) {
+            download.file("https://d396qusza40orc.cloudfront.net
+                          /exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                          destfile=zipname, method="curl")                        
+        }
+        unzip(zipname)
+    }
+    data <- read.table(filename, header=T, sep=";", stringsAsFactors=F)
+    
+    # convert Dates into date format, Times into time, subset to just the
+    # 2 days we care about, merge Date and Time into a single DateTime column,
+    # and force numeric values for the other measurements.
+    library(lubridate)
+    data$Date <- dmy(data$Date)
+    data$Time <- hms(data$Time)
+    data <- data[data$Date %in% c(dmy(01022007), dmy(02022007)), ]
+    data <- cbind(data[,1] + data[,2], data[,3:9]) # now there is one less
+    names(data)[1] <- "DateTime"                       # column.
+    data[,2:8] <- apply(data[,2:8], 2, as.numeric)
+        
+    # construct plot1
+    png(filename="plot2.png")
+    plot(data$DateTime, data$Global_active_power, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    dev.off()
+}
---
+++ plot4.R
@@ -0,0 +1,45 @@
+plot4 <- function() {
+    # Loads the data from the zip file url and reproduces plot4.png
+        
+    # If the zip file is not in the present working directory, download it.
+    # Then unzip (if necessary), and read the data into the dataframe data.
+    filename <- "household_power_consumption.txt"
+    zipname <- "exdata-data-household_power_consumption.zip"
+    if(!file.exists(filename)) {
+        if(!file.exists(zipname)) {
+            download.file("https://d396qusza40orc.cloudfront.net
+                          /exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                          destfile=zipname, method="curl")                        
+        }
+        unzip(zipname)
+    }
+    data <- read.table(filename, header=T, sep=";", stringsAsFactors=F)
+    
+    # convert Dates into date format, Times into time, subset to just the
+    # 2 days we care about, merge Date and Time into a single DateTime column,
+    # and force numeric values for the other measurements.
+    library(lubridate)
+    data$Date <- dmy(data$Date)
+    data$Time <- hms(data$Time)
+    data <- data[data$Date %in% c(dmy(01022007), dmy(02022007)), ]
+    data <- cbind(data[,1] + data[,2], data[,3:9]) # now there is one less
+    names(data)[1] <- "DateTime"                       # column.
+    data[,2:8] <- apply(data[,2:8], 2, as.numeric)
+        
+    # construct plot4
+    png(filename="plot4.png")
+    par(mfrow=c(2, 2))
+    plot(data$DateTime, data$Global_active_power, type="l",
+         ylab="Global Active Power", xlab="")
+    plot(data$DateTime, data$Voltage, type="l",
+         ylab="Voltage", xlab="datetime")
+    plot(data$DateTime, data$Sub_metering_1, type="l",
+         ylab="Energy sub metering", xlab="")
+    lines(data$DateTime, data$Sub_metering_2, col="red")
+    lines(data$DateTime, data$Sub_metering_3, col="blue")
+    legend("topright", lty=1, bty="n", col=c("black", "red", "blue"), 
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    plot(data$DateTime, data$Global_reactive_power, type="l",
+         ylab="Global_reactive_power", xlab="datetime")
+    dev.off()
+}
